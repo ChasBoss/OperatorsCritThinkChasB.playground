875ddf967d9057dcaf7c0aa7bbad92a9f547fcbd
ChasBoss <white@Whites-Mac-mini.hsd1.il.comcast.net>
2015-09-14 18:12:05 -0500

    First
    first

---
+++ Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var char = (1,2,4,6)
+
+println(char)
+
+let numberPulled = char
+
+println(numberPulled)
+
+
+var num1 = 1
+var num2 = 34
+var num3 = 6245
+var letters = "hello"
+
+num3 % 5
+num1++
+num2 - num1 * 5 / num1
+
+var something:Int? = 100
+
+if (something != nil){
+    println("equals 100")
+}
+else
+{
+    println("does not equal 100")
+}
+
+num1 = num3
+if num1 == num3{
+    num1++
+}
+else
+{
+    num3++
+}
+
+println(num3)
+println(num1)
+
+num1 == num3
